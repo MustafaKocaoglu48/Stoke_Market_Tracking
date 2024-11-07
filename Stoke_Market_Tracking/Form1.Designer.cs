70fb3cc1e817522514f7ebf05d2901a7905fa74d
Mustafa <101017438+MustafaKocaoglu48@users.noreply.github.com>
2024-11-07 16:20:53 +0300

    Add project files.

---
+++ Stoke_Market_Tracking/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Stoke_Market_Tracking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbDoviz = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbDoviz
+            // 
+            this.cmbDoviz.FormattingEnabled = true;
+            this.cmbDoviz.Items.AddRange(new object[] {
+            ""});
+            this.cmbDoviz.Location = new System.Drawing.Point(266, 69);
+            this.cmbDoviz.Name = "cmbDoviz";
+            this.cmbDoviz.Size = new System.Drawing.Size(121, 24);
+            this.cmbDoviz.TabIndex = 0;
+            this.cmbDoviz.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(384, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Stoke_Market_Tracking.Properties.Resources.borsa_ogrenmek_istiyorum_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbDoviz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbDoviz;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Stoke_Market_Tracking/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Stoke_Market_Tracking
+{
+
+    public partial class Form1 : Form
+    {
+        public int userId;
+        public string dolarAlis;
+        public string dolarSatis;
+        public string euroAlis;
+        public string euroSatis;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.userId = userId;
+            this.Load += new System.EventHandler(this.Form1_Load);
+
+        }
+        private void doviz_Load()
+        {
+            string bugun = "https://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmldosya = new XmlDocument();
+            xmldosya.Load(bugun);
+
+            dolarAlis = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteBuying").InnerXml;
+            dolarSatis = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+            euroAlis = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteBuying").InnerXml;
+            euroSatis = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            doviz_Load();
+
+            List<string> dovizKurlari = new List<string> { "USD", "EUR" };
+            cmbDoviz.Items.AddRange(dovizKurlari.ToArray());
+
+            cmbDoviz.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string secilenDoviz = cmbDoviz.SelectedItem.ToString();
+            label1.Text = secilenDoviz;
+
+            switch (secilenDoviz)
+            {
+                case "USD":
+                    label2.Text = "Alış: " + dolarAlis;
+                    label3.Text = "Satış: " + dolarSatis;
+                    break;
+                case "EUR":
+                    label2.Text = "Alış: " + euroAlis;
+                    label3.Text = "Satış: " + euroSatis;
+                    break;
+                default:
+                    label2.Text = "Alış: Bilinmiyor";
+                    label3.Text = "Satış: Bilinmiyor";
+                    break;
+            }
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
